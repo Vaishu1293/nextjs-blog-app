7f53c0fa6acff5b3d775a9e9d727323c511978e0
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-16 16:40:00 +0000

    image-optimization-cloudinary

--- components/posts.js
+++ components/posts.js
@@ -6,11 +6,18 @@ import { togglePostLikeStatus } from '@/actions/posts';
 import { useOptimistic } from 'react';
 import Image from 'next/image';
 
+function imageLoader(config){
+  const urlStart = config.src.split('upload/')[0];
+  const urlEnd = config.src.split('upload/')[1];
+  const transformations = `w_200,q_${config.quality}`;
+  return `${urlStart}upload/${transformations}/${urlEnd}`;
+}
+
 function Post({ post, action }) {
   return (
     <article className="post">
       <div className="post-image">
-        <Image src={post.image} fill alt={post.title}/>
+        <Image src={post.image} fill alt={post.title} loader={imageLoader} quality={50}/>
       </div>
       <div className="post-content">
         <header>
